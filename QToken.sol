e2ee1f241235374497a9b5be9eae2b7dc4ea2166
qulzamsyed <bsef17m017@pucit.edu.pk>
2021-09-17 15:09:49 +0500

    Add Interfaces and implement the safeMath

--- QToken.sol
+++ QToken.sol
@@ -1,13 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+interface IQToken{
+    function getName() external view returns(string memory);
+    function getSymbol() external view returns(string memory);
+    function getTotalSupply() external view returns(uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient,uint256 amount) external;
+    function transferFrom(address sender,address recipient,uint256 amount ) external returns (bool);
+    function approve(address _owner,address _spender,uint256 _amount) external;
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function mint(address account, uint256 amount)  external ;
+    function burn(address account, uint256 amount)  external ;
+}
 contract QToken {
+    using SafeMath for uint;
     mapping(address => uint256) private balances;
     mapping(address => mapping(address => uint256)) private allowances;
     uint256 private totalSupply;
     string private name;
     string private symbol;
     address public owner;
-    constructor (string memory _name, string memory _symbol,uint256 _totalSupply) public{
+    constructor (string memory _name, string memory _symbol,uint256 _totalSupply){
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply;
@@ -17,48 +31,48 @@ contract QToken {
     require(msg.sender == owner,"This function is restricted to the contract's owner");
     _;
   }
-    function getName() public view returns(string memory) {
+    function getName() external view returns(string memory) {
         return name;
     }
-    function getSymbol() public view returns(string memory) {
+    function getSymbol() external view returns(string memory) {
         return symbol;
     }
-    function getTotalSupply() public view returns(uint256) {
+    function getTotalSupply() external view returns(uint256) {
         return totalSupply;
     }
-     function balanceOf(address account) public view returns (uint256) {
+     function balanceOf(address account) external view returns (uint256) {
         return balances[account];
     }
-    function transfer(address recipient,uint256 amount) public{
+    function transfer(address recipient,uint256 amount) external{
         uint256 senderBalance = balances[msg.sender];
         require(senderBalance >= amount, "  ");
-            balances[msg.sender] = senderBalance - amount;
-            balances[recipient] += amount;
+            balances[msg.sender] = senderBalance.sub(amount);
+            balances[recipient] = balances[recipient].add(amount);
     }
-    function transferFrom(address sender,address recipient,uint256 amount ) public returns (bool) {
+    function transferFrom(address sender,address recipient,uint256 amount ) external returns (bool) {
             require(balances[sender] >= amount, "transfer amount exceeds balance");
             require(allowances[sender][msg.sender] >= amount, "transfer amount exceeds allowance");
-            balances[sender] = balances[sender] - amount;
+            balances[sender] = balances[sender].sub(amount);
             balances[recipient] += amount;
-            allowances[sender][msg.sender] -= amount;
+            allowances[sender][msg.sender] = allowances[sender][msg.sender].sub(amount);
         return true;
     }
-      function approve(address _owner,address _spender,uint256 _amount) public {
+      function approve(address _owner,address _spender,uint256 _amount) external {
         allowances[_owner][_spender] = _amount;
     }
-    function allowance(address _owner, address _spender) public view returns (uint256) {
+    function allowance(address _owner, address _spender) external view returns (uint256) {
         return allowances[_owner][_spender];
     }
-    function mint(address account, uint256 amount)  public onlyContractOwner{
+    function mint(address account, uint256 amount)  external onlyContractOwner{
 
-        totalSupply += amount;
-        balances[account] += amount;
+        totalSupply = totalSupply.add(amount);
+        balances[account] = balances[account].add(amount);
     }
-    function burn(address account, uint256 amount)  public onlyContractOwner{
+    function burn(address account, uint256 amount)  external onlyContractOwner{
 
         require(balances[account] >= amount, "burn amount exceeds balance");
-            balances[account] = balances[account] - amount;
-            totalSupply -= amount;
+            balances[account] = balances[account].sub(amount);
+            totalSupply = totalSupply.sub(amount);
     }
 
 
